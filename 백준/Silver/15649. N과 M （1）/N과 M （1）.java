8ce9084faf2e2cc230876435327202c4316b4c13
honggyeonghyeon <156874613+honggyeonghyeon@users.noreply.github.com>
2024-01-30 14:27:55 +0900

    [Silver III] Title: N과 M (1), Time: 2256 ms, Memory: 61420 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,14 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M;
 	static boolean isUsed[];
 	static int[] per;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());;
+		M = Integer.parseInt(st.nextToken());;
 		isUsed = new boolean[N+1];
 		per = new int[M];
 		permu(0);
