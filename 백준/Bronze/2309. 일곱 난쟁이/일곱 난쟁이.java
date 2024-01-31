4dcd3913e7e0dbaca68d60f6ab97b67a7a4b8748
honggyeonghyeon <156874613+honggyeonghyeon@users.noreply.github.com>
2024-01-31 14:10:52 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 72 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -4,40 +4,34 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-	static int[] height = new int[9]; //아홉 난쟁이들의 키
-	static int[] result = new int[7]; //일곱 난쟁이들의 키
-	static boolean[] isSelected = new boolean[9]; //선택된 난쟁이
-	static boolean found; //키의 합이 100인 경우
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] height = new int[9];
+		int sum = 0;
 		for(int i=0; i<9; i++) {
 			height[i] = Integer.parseInt(br.readLine());
+			sum += height[i];
 		}
-		
 		Arrays.sort(height);
-		comb(0, 0);
+		int a = -1, b=-1;
+		int result = sum;
 		
-	}
-	
-	private static void comb(int cnt, int start) {
-		if(found) return;
-		if(cnt == 7) {
-			if(Arrays.stream(result).sum()==100){
-				for (int i = 0; i < 7; i++) {
-					System.out.println(result[i]);
+		A: for(int i=0; i<9; i++) {
+			for(int j=1; j<9; j++) {
+				result -= (height[i] + height[j]);
+				if(result==100) {
+					a = i;
+					b = j;
+					break A;
 				}
-				found = true;
+				result = sum;
 			}
-			return;
 		}
-		for(int i=start; i<9; i++) {
-			if(isSelected[i]) continue;
-			result[cnt] = height[i];
-			isSelected[i] = true;
-			comb(cnt+1, start+1);
-			isSelected[i] = false;
+		
+		for(int i=0; i<9; i++) {
+			if(i==a || i==b) continue;
+			System.out.println(height[i]);
 		}
 	}
-
 }
