0cab4b0fefd63ba2eb48adc44c71426b5cdd9fc6
honggyeonghyeon <156874613+honggyeonghyeon@users.noreply.github.com>
2024-02-02 14:46:32 +0900

    [Silver IV] Title: 카드2, Time: 136 ms, Memory: 31636 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Queue<Integer> q = new ArrayDeque<>();
+		for(int i=1; i<=N; i++)
+			q.offer(i);
+		while(true) {
+			if(q.size()==1) {
+				System.out.println(q.poll());
+				break;
+			}
+			q.poll();
+			int n = q.poll();
+			q.offer(n);
+		}
+	}
+}
