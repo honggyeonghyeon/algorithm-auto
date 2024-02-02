3ba54120c43fb7275b57c858a2a0bc831bdafb12
honggyeonghyeon <156874613+honggyeonghyeon@users.noreply.github.com>
2024-02-02 14:53:49 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 23808 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -4,21 +4,29 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+/* 31,636KB 136ms
+ * 문제 해석
+	N장의 카드 1 → N번 카드 순
+	제일 위에 있는 카드를 버리고 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮김
+	
+	입력
+	N
+	
+	출력
+	첫번째 줄에 남게 되는 카드의 번호
+ */
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		Queue<Integer> q = new ArrayDeque<>();
+		Queue<Integer> q = new ArrayDeque<Integer>();
 		for(int i=1; i<=N; i++)
 			q.offer(i);
-		while(true) {
-			if(q.size()==1) {
-				System.out.println(q.poll());
-				break;
-			}
+		while(q.size()>1) {
 			q.poll();
-			int n = q.poll();
-			q.offer(n);
+			q.offer(q.poll());
 		}
+		System.out.println(q.poll());
 	}
 }
