5695350ba427847de25b31b98ae7e1e5d2d84614
honggyeonghyeon <156874613+honggyeonghyeon@users.noreply.github.com>
2024-02-07 15:07:40 +0900

    [Silver I] Title: 절댓값 힙, Time: 708 ms, Memory: 26752 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
 	static class Heap implements Comparable<Heap>{
@@ -30,7 +28,6 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
 		int N = Integer.parseInt(br.readLine());
 		for(int i=0; i<N; i++) {
 			int num = Integer.parseInt(br.readLine());
