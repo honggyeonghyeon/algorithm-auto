05ec18abaca2e375adff0c4e666890046cfbeeed
honggyeonghyeon <156874613+honggyeonghyeon@users.noreply.github.com>
2024-02-13 11:49:02 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 11484 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -3,23 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int result = 0;
-		
-		for(int i=n/5; i>=0; i--) {
-			result = i;
-			if((n-i*5)%3==0) {
-				result += (n-i*5)/3;
-				System.out.println(result);
-				return;
-			}
+		int N = Integer.parseInt(br.readLine());
+		int dp[] = new int[N+1];
+		dp[N] = -1;
+		dp[3] = 1;
+		if(N>=5) dp[5] = 1;
+
+		for(int i=6; i<=N; i++) {
+			if(dp[i-3]==0) {
+				if(dp[i-5]==0) continue;
+				dp[i] = dp[i-5]+1;
+			}else if(dp[i-5]==0)dp[i] = dp[i-3]+1;
+			else dp[i] = Math.min(dp[i-3]+1, dp[i-5]+1);
 		}
 		
-		System.out.println(-1);
+		System.out.println(dp[N]);
 	}
 
 }
